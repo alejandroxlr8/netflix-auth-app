0c6bdb7abec097027bbcdfadfee1d70ccaa829d9
unknown <correoalex8@gmail.com>
2025-07-20 15:27:07 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
+import Login from './Login';
+import MovieGallery from './MovieGallery';
 
 function App() {
+  const [token, setToken] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!token ? (
+        <Login setToken={setToken} />
+      ) : (
+        <MovieGallery token={token} />
+      )}
     </div>
   );
 }
